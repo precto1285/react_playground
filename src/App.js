efec4d40c2428b5c2e767d9f1b545644a8abc16c
Philippe Recto <precto1285@gmal.com>
2019-07-02 15:21:41 -0700

    created component and added jumbotron and table

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import CalendarDay from './components/CalendarDay';
+import { Jumbotron, Table } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "Calendar Day"
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Jumbotron className="bg-info text-white">
+          <h1>Calendar</h1>
+          <h5>Weekly Schedule</h5>
+        </Jumbotron>
+        <Table bordered>
+          <thead>
+            <tr>
+              <th>Sunday</th>
+              <th>Monday</th>
+              <th>Tuesday</th>
+              <th>Wednesday</th>
+              <th>Thursday</th>
+              <th>Friday</th>
+              <th>Saturday</th>
+            </tr>
+          </thead>
+        </Table>
+
+
+        <CalendarDay title={this.state.title} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/CalendarDay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CalendarDay(props) {
+  return (
+    <div>
+      <h3>{props.title}</h3>
+    </div>
+  );
+}
+
+
+export default CalendarDay;
